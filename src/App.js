350a72b6f8f9ad9e61fababe33008a5c7cec1ea6
RANJITHMEENA <ranjithkumarproton@gmail.com>
2024-02-10 10:12:21 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+// app.js
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link  , useParams,Routes} from 'react-router-dom';
+
+const UserListPage = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://api.github.com/users');
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching users:', error.message);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users?.map((user) => (
+          <li key={user.id}>
+            <Link to={`/user/${user?.login}`}>
+              <img src={user?.avatar_url} alt={`${user.login}'s avatar`} style={{width:"3rem" , borderRadius:"2rem"}} />
+              ({user.login})
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+const UserDetailsPage = ({ match }) => {
+  const params = useParams()
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const username = params?.username;
+    const fetchUserDetails = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.error('Error fetching user details:', error.message);
+      }
+    };
+
+    fetchUserDetails();
+  }, [params?.username]);
+
+  return (
+    <div>
+      <h1>User Details</h1>
+      {user && (
+        <div>
+          <img src={user?.avatar_url} alt={`${user?.login}'s avatar`}  />
+          <p>Name: {user?.name ? user.name : 'N/A'}</p>
+          <p>Company:{user?.company}</p>
+          <p>BIO:{user?.bio === null ? "N/A" : user?.bio}</p>
+          <p>Login: {user?.login}</p>
+          <p>organizations-url : {user?.organizations_url}</p>
+          {/* Add more details as needed */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<UserListPage />} />
+      <Route path="/user/:username" element={<UserDetailsPage />} />
+    </Routes>
+  </Router>
+  );
+};
 
 export default App;
